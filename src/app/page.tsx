3586c8bcbbcffc4cc55a0d46d919a16388a75636
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-03-31 20:34:43 -0300

    feat: connect to API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,65 @@
-import { Team } from "../../types";
+import TeamCard from '@/components/TeamCard/TeamCard';
+import { Team } from '../../types';
 
-type season = "2024-2025" | "2023-2024" | "2022-2023"
+type season = '2024-2025' | '2023-2024' | '2022-2023';
 
 const getActualSeason = () => {
   const actualYear = new Date().getFullYear();
-  return `${actualYear-1}-${actualYear}` as season
-}
+  return `${actualYear - 1}-${actualYear}` as season;
+};
 
-const getNbaTeams = async (season:season, leagueId: string) => {
-    if (!process.env.API_url || !process.env.API_key || !process.env.API_host) {
-      throw new Error('API configuration is missing');
-    } else if (!season || !leagueId) {
-      throw new Error('Year and league are required');
+const getNbaTeams = async (season: season, leagueId: string) => {
+  if (!process.env.API_url || !process.env.API_key || !process.env.API_host) {
+    throw new Error('API configuration is missing');
+  } else if (!season || !leagueId) {
+    throw new Error('Year and league are required');
+  }
+
+  const url = `${process.env.API_url}/teams?league=${leagueId}&season=${season}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': process.env.API_key,
+      'x-rapidapi-host': process.env.API_host,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result: {
+      errors: any[] | { token: string };
+      result: number;
+      response: Team[];
+    } = await response.json().then((data) => data);
+
+    if (!Array.isArray(result.errors)) {
+      throw new Error(`${result.errors.token}`);
     }
 
-    const url = `${process.env.API_url}/teams?league=${leagueId}&season=${season}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': process.env.API_key,
-        'x-rapidapi-host': process.env.API_host
-      }
-    };
+    console.log(result.response);
 
-    try {
-      const response = await fetch(url, options);
-      const result:{
-        errors: any[] | {token: string},
-        result: number,
-        response: Team[]
-      } = await response.json().then((data) => data);
-
-      if (!Array.isArray(result.errors)) {
-        throw new Error(`${result.errors.token}`);
-      }
-
-      console.log(result.response)
-
-      return result.response
-
-    } catch (error) {
-      console.error(error);
-      throw new Error('Error fetching data');
-    }
-}
-
+    return result.response;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error fetching data');
+  }
+};
 
 export default async function Home() {
-  const NbaTeams: Team[] = await getNbaTeams(getActualSeason(), '12');
+  const NbaTeams: Team[] = (await getNbaTeams(getActualSeason(), '12')).filter(
+    (team) => team.name !== 'West' && team.name !== 'East',
+  );
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {
-        NbaTeams.map((team: Team) => (
-          <div key={team.id} className="flex flex-col items-center justify-center">
-            <h1 className="text-2xl font-bold">{team.name}</h1>
-            <div className="h-24 w-30 aspect-auto">
-              <img className="w-full h-full border" src={team.logo || ''} alt={team.name} />
-            </div>
-          </div>
-        ))
-      }
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-10">
+      <h1 className="text-4xl font-bold bg-gradient bg-350 bg-0 pb-20 text-gradient">
+        NBA Teams
+      </h1>
+      <section className="flex gap-2 flex-wrap">
+        {NbaTeams.map((team: Team) => (
+          <TeamCard key={team.id} team={team} />
+        ))}
+      </section>
     </main>
   );
 }
--- src/components/TeamCard/TeamCard.tsx
+++ src/components/TeamCard/TeamCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Team } from '../../../types';
+import { Card, CardContent, CardTitle } from '../ui/card';
+import Link from 'next/link';
 
 type Props = {
   team: Team;
@@ -7,16 +9,23 @@ type Props = {
 
 const TeamCard = ({ team }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">{team.name}</h1>
-      <div className="h-24 w-30 aspect-auto">
-        <img
-          className="w-full h-full border"
-          src={team.logo || ''}
-          alt={team.name}
-        />
-      </div>
-    </div>
+    <Link
+      href={`/${team.name}`}
+      className="hover:opacity-75 transition-opacity"
+    >
+      <Card className="flex flex-col gap-2 items-center">
+        <CardTitle className="">{team.name}</CardTitle>
+        <CardContent>
+          <div className=" h-24 w-28">
+            <img
+              className="w-full h-full aspect-square"
+              src={team.logo || ''}
+              alt={team.name}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
