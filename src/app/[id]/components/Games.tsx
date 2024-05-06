e1d5839030251d989a6916e64abbcba61a210b8b
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-05-05 21:51:36 -0300

    fix: bug with the score of the playoff serie

--- src/app/[id]/components/Games.tsx
+++ src/app/[id]/components/Games.tsx
@@ -20,12 +20,18 @@ const Games = async ({ teamId }: Props) => {
   ).then((games) => games.filter((game) => game.status === 'Final').reverse());
 
   const postSeasonPastGames = pastGames.filter((game) => game.postseason);
+  const LastPostSeasonTeamIds = [
+    postSeasonPastGames[0].visitor_team.id,
+    postSeasonPastGames[0].home_team.id,
+  ];
 
-  const countGamesPerTeam = pastGames.reduce(
+  const countGamesPerTeam = postSeasonPastGames.reduce(
     (acc: Map<string, number>, game: Game) => {
       if (
         game.visitor_team_score === null ||
         game.home_team_score === null ||
+        !LastPostSeasonTeamIds.includes(game.home_team.id) ||
+        !LastPostSeasonTeamIds.includes(game.visitor_team.id) ||
         (acc.size === 2 &&
           (!acc.has(String(game.visitor_team.id)) ||
             !acc.has(String(game.home_team.id))))
@@ -46,7 +52,9 @@ const Games = async ({ teamId }: Props) => {
       }
       return acc;
     },
-    new Map<string, number>(),
+    new Map<string, number>(
+      LastPostSeasonTeamIds.map((teamId) => [String(teamId), 0]),
+    ),
   );
 
   return (
